de5cd5ece177a6d9f92e431f320981ccfb36d201
Unknown <empireishere@vortexx.cf>
2017-04-04 18:34:20 +0200

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Mar 8 14:31:54 ICT 2017"
+#define LINUX_COMPILE_BY "vn_kernel"
+#define LINUX_COMPILE_HOST "SEV-BUILD-18"
+#define LINUX_COMPILER "gcc version 4.9 20140514 (prerelease) (GCC) "
